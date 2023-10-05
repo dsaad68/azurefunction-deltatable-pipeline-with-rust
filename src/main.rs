3b421e8aa38870a682696b45a9e4888d37228fae
Daniel Saad <dsaad68@users.noreply.github.com>
2023-10-05 22:48:32 +0200

    - added serde for json - added timezone

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,86 @@
+use std::env;
+use std::convert::Infallible;
+
+use chrono::prelude::*;
+use chrono::NaiveDateTime;
+use chrono_tz::Europe::Berlin;
+
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{header, Body, Error, Request, Response, Server, StatusCode};
-use std::convert::Infallible;
-use std::env;
-use serde_json::Value;
-use serde_json::json;
+
+use serde_json::{Value, json};
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct EventData {
+    api: String,
+    #[serde(rename = "contentType")]
+    content_type: String,
+    #[serde(rename = "contentLength")] 
+    content_length: i64,
+    #[serde(rename = "blobType")] 
+    blob_type: String,
+    #[serde(rename = "blobUrl")] 
+    blob_url: String,
+    url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct EventGridEvent {
+    data: EventData,
+    #[serde(rename = "eventTime")] 
+    event_time: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    #[serde(rename = "eventGridEvent")] 
+    event_grid_event: EventGridEvent,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Root {
+    #[serde(rename = "Data")]
+    data: Data
+}
+
+#[derive(Debug)]
+struct BlobEvent {
+    api: String,
+    content_type: String,
+    content_length: i64,
+    blob_type: String,
+    blob_url: String,
+    url: String,
+    event_time: NaiveDateTime
+}
+
+
+impl From<Root> for BlobEvent {
+    fn from(root: Root) -> Self {
+        let event_data = root.data.event_grid_event.data;
+        let event_time = DateTime::parse_from_rfc3339(root.data.event_grid_event.event_time.as_str())
+        .unwrap()
+        .with_timezone(&Berlin)
+        .naive_local();
+        
+        BlobEvent {
+            api: event_data.api,
+            content_type: event_data.content_type,
+            content_length: event_data.content_length,
+            blob_type: event_data.blob_type,
+            blob_url: event_data.blob_url,
+            url: event_data.url,
+            event_time
+        }
+    }
+}
 
 async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+
+    println!("Rust HTTP trigger function begun");
+
     // Read the request body as bytes.
     let body_bytes = match hyper::body::to_bytes(req.into_body()).await {
         Ok(bytes) => bytes,
@@ -17,8 +92,6 @@ async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
                 .unwrap())
         }
     };
-
-    println!("{:?}", body_bytes);
 
     // Check if the Body is Empty
     // Azure function code 204
@@ -37,12 +110,12 @@ async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
 
     match json_request {
         Ok(json_request) => {
-            // Extract the "Metadata" part.
-            let metadata = json_request.get("Metadata").unwrap_or(&Value::Null);
 
-            // Convert metadata back to JSON and print it.
-            let metadata_json = serde_json::to_string(&metadata).unwrap();
-            println!("Metadata: {}", metadata_json);
+            let deserialized_data: Root = serde_json::from_str(&json_request.to_string()).unwrap();
+
+            // Convert the Root instance to a BlobEvent instance
+            let blob_event = BlobEvent::from(deserialized_data);
+            println!("{:?}", blob_event);
 
             let response = Response::builder()
                 .status(StatusCode::OK)
@@ -64,7 +137,6 @@ async fn handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
         }
     }
 }
-
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
